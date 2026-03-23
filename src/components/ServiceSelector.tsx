f562dbd287247571a3df7195527631f9e82ee1e0
عزالدين ناصر <121424065+izzeldeenn@users.noreply.github.com>
2026-03-23 11:00:44 +0200

    add

--- src/components/ServiceSelector.tsx
+++ src/components/ServiceSelector.tsx
@@ -270,7 +270,7 @@ export function ServiceSelector() {
     <div className="w-full h-screen flex flex-col overflow-hidden">
       {/* Active Timer - Scrollable Container */}
       <div className="flex-1 overflow-hidden">
-        <div className="h-full overflow-y-auto">
+        <div className="h-full overflow-y-auto flex items-center justify-center">
           {renderTimer()}
         </div>
       </div>
